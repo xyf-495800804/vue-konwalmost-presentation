dd06442182987c170cd1fb3e46e48fb4cb9873a1
螃蟹岬是不是螃蟹很多 <495800804@qq.com>
2021-01-12 21:47:13 +0800

    入口文件配置

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+/* 全局引入elementUI */
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
+/* 引入pc端适配 */
+import "./utils/rem";
+/* 引入iconfont字体图标库 */
+import "./assets/iconfont/iconfont.css";
+/* 引入公共样式 */
+import "./assets/css/public.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
