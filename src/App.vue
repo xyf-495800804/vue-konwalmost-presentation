eb9dbf2d0599f21a252818223b9bb41456d111fe
螃蟹岬是不是螃蟹很多 <495800804@qq.com>
2021-01-12 21:46:44 +0800

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,17 @@
 </template>
 
 <style lang="less">
+html,
+body,
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background: #f6f6f6;
 }
 </style>
